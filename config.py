0a71cdf92cc5db88cc8d3ebeb9748253688df8f2
DjMixpur2 <djmixpur2@gmail.com>
2024-11-26 18:15:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178732396:AAEd104z69Wv0xQel_fgdWONINLHbKRBRIg")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "770434685")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Kdbhai:Kdbhai@cluster0.qxasr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "Forward")   
