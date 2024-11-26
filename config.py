630ac56028f5788dd0de36069ef6ef5c553b83fe
DjMixpur2 <djmixpur2@gmail.com>
2024-11-26 18:06:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "15564ec4a1a2cbef87c99a9aa9e40b34")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178732396:AAEd104z69Wv0xQel_fgdWONINLHbKRBRIg")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    OWNER_ID = os.environ.get("OWNER_ID", "770434685")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
+    SESSION = os.environ.get("SESSION", "Forward")   
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1002260661088"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "MovieJungle_Bot")
 
 
 def LOGGER(name: str) -> logging.Logger:
